09298b7edffa60ba8b4e2b91b952bb3e92283baa
Yago <yagomateos@hotmail.com>
2023-04-26 09:58:41 +0200

    fucntions as props fixed no eslint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-// eslint-disable-next-line no-unused-vars
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// eslint-disable-next-line no-unused-vars
 import App from "./App";
 
 ReactDOM.render(
