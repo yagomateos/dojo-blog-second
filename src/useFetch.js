f8307374914954b60b551b7748b84671b187c3b8
Yago <yagomateos@hotmail.com>
2023-04-26 20:50:32 +0200

    useEffect CleanUp

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        const abortCont = new AbortController();
+
     setTimeout(() => {
-    fetch(url)
+    fetch(url, { signal: abortCont.signal })
     .then(res => {
         if (!res.ok) {
         throw Error('could not fetch the data for that resource');
@@ -20,11 +22,20 @@ const useFetch = (url) => {
         setError(null);
         })
         .catch(err => {
+        if (err.name === 'AbortError') {
+        console.log('fetch aborted');
+        } else {
+        setIsPending(false);
+        setError(err.message);
+        }
+
         setError(err.message);
         setIsPending(false);
         })
     },  1000);
-},[]);
+    return () => abortCont.abort();
+
+},[url]);
     return { data, isPending, error }
 }
 
