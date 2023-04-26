ef2518c6c4da5e756d03fdd1db0a12392407ef58
Yago <yagomateos@hotmail.com>
2023-04-26 08:51:37 +0200

    functions as props

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,22 @@
-import { useState } from 'react';
-import BlogList from './BlogList';
+import { useState } from "react";
+import BlogList from "./BlogList";
 
-const Home= () => {
-
+const Home = () => {
     const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
+    const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+    }
 
-        ]);
-
-    return (
+return (
     <div className="home">
-        <BlogList blogs={blogs} title = "All Blogs!"  />
-            </div>
-
+        <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+    </div>
 );
 }
 
