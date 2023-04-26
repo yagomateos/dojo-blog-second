9066d3d0a53b6ebaf170b1f03cbb22c055bdd0f3
Yago <yagomateos@hotmail.com>
2023-04-26 10:28:21 +0200

    useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -13,6 +13,12 @@ const Home = () => {
     setBlogs(newBlogs);
   }
 
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(blogs);
+  }
+  );
+  
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
