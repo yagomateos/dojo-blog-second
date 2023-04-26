fe0ac4f2da44ec7308aa0080e9fc2f29024c6e63
Yago <yagomateos@hotmail.com>
2023-04-26 09:37:45 +0200

    reusing components

--- src/Home.js
+++ src/Home.js
@@ -17,6 +17,7 @@ const Home = () => {
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs.filter((blog) => blog.author === "mario")} title="Mario´s Blogs" />
     </div>
   );
 };
